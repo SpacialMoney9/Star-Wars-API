738fb754875869dab5e5b208336746d176d739ed
Om Sahoo <alice@webage.com>
2024-09-23 16:26:42 -0400

    films and characters list

--- planet.js
+++ planet.js
@@ -9,11 +9,8 @@ const baseUrl = `http://localhost:9001/api`;
 // Runs on page load
 addEventListener('DOMContentLoaded', () => {
   nameH1 = document.querySelector('h1#name');
-//   birthYearSpan = document.querySelector('span#birth_year');
-//   massSpan = document.querySelector('span#mass');
-//   heightSpan = document.querySelector('span#height');
-//   homeworldSpan = document.querySelector('span#homeworld');
-//   filmsUl = document.querySelector('#films>ul');
+  charactersUl = document.querySelector('#characters>ul');
+  filmsUl = document.querySelector('#films>ul');
   const sp = new URLSearchParams(window.location.search)
   const id = sp.get('id')
   getPlanet(id)
@@ -23,8 +20,8 @@ async function getPlanet(id) {
   let planet;
   try {
     planet = await fetchPlanet(id)
-    // planet.homeworld = await fetchHomeworld(planet)
-    // planet.films = await fetchFilms(planet)
+    planet.characters = await fetchCharacters(planet)
+    planet.films = await fetchFilms(planet)
   }
   catch (ex) {
     console.error(`Error reading planet ${id} data.`, ex.message);
@@ -38,27 +35,25 @@ async function fetchPlanet(id) {
     .then(res => res.json())
 }
 
-// async function fetchHomeworld(planet) {
-//   const url = `${baseUrl}/planets/${planet?.homeworld}`;
-//   const planet = await fetch(url)
-//     .then(res => res.json())
-//   return planet;
-// }
+async function fetchCharacters(planet) {
+  const url = `${baseUrl}/planets/${planet?.id}/characters`;
+  const characters = await fetch(url)
+    .then(res => res.json())
+  return characters;
+}
 
-// async function fetchFilms(planet) {
-//   const url = `${baseUrl}/planets/${planet?.id}/films`;
-//   const films = await fetch(url)
-//     .then(res => res.json())
-//   return films;
-// }
+async function fetchFilms(planet) {
+  const url = `${baseUrl}/planets/${planet?.id}/films`;
+  const films = await fetch(url)
+    .then(res => res.json())
+  return films;
+}
 
 const renderPlanet = planet => {
-  document.title = `SWAPI - ${planet?.name}`;  // Just to make the browser tab say their name
+  document.title = `SWAPI - ${planet?.name}`;
   nameH1.textContent = planet?.name;
-//   heightSpan.textContent = planet?.height;
-//   massSpan.textContent = planet?.mass;
-//   birthYearSpan.textContent = planet?.birth_year;
-//   homeworldSpan.innerHTML = `<a href="/planet.html?id=${planet?.homeworld.id}">${planet?.homeworld.name}</a>`;
-//   const filmsLis = planet?.films?.map(film => `<li><a href="/film.html?id=${film.id}">${film.title}</li>`)
-//   filmsUl.innerHTML = filmsLis.join("");
+  const charactersLis = planet?.characters?.map(character => `<li><a href="/character.html?id=${character.id}">${character.name}</li>`)
+  charactersUl.innerHTML = charactersLis.join("");  
+  const filmsLis = planet?.films?.map(film => `<li><a href="/film.html?id=${film.id}">${film.title}</li>`)
+  filmsUl.innerHTML = filmsLis.join("");
 }
